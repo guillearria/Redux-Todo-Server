728572d30f08bcba15203a7e5900dd6e638ebf3b
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-08-29 14:09:31 -0700

    configured todos seed

--- database/seeds/001-todos.js
+++ database/seeds/001-todos.js
@@ -1,13 +1,9 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('todos').insert([
+    {id: 1, item: 'Walk dog', completed: false},
+    {id: 2, item: 'Take out trash', completed: false},
+    {id: 3, item: 'Cook dinner', completed: false}
+  ]);
 };
