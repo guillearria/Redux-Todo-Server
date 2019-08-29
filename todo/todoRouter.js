b6ddeea8dbe487e1cec926324f0dae8e635c1b66
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-08-29 15:36:22 -0700

    Configured POST todos endpoint

--- todo/todoRouter.js
+++ todo/todoRouter.js
@@ -1,19 +1,29 @@
 const express = require("express")
 
-const todos = require("../todo/todoModel")
+const db = require("../todo/todoModel")
 
 const router = express.Router()
 
 router.get("/", async (req,res) => {
   try {
-    const todos = await todos.getAll()
+    const todos = await db.getAll()
     
-    res.status(200).json(users)
+    res.status(200).json({ todos })
   } catch (error) {
     res.status(500).json({ error: "Server Error"})
   }
 })
 
 router.post("/", async (req, res) => {
+  try {
+    if (req.body.item) {
+      const todos = await db.insert()
   
+      res.status(201).json({ todos })
+    } else {
+      res.status(400).json({ error: "Todo name required."})
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server Error"})
+  }
 })
